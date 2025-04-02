7954d690c8e28467e72c524a7d7ef6e9bff9efc4
kantkamal88 <rickyrohila02@gmail.com>
2025-04-02 12:46:43 +0530

    Update A.java

--- A.java
+++ A.java
@@ -2,6 +2,6 @@ class A{
 int a=10;
 int b=20;
 	public static void main(String args[]){
-	System.out.println("A- "+a+"+   B- "+b+" = "+(a+b));
+	System.out.println("A- "+a+"-   B- "+b+" = "+(a+b));
 	}
 }
